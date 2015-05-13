4f2696cd7c0cee1950bd6c355cfc27d0a663d459
Garcia Bermudez, G <g.garcia.bermudez@accenture.com>
2015-05-13 12:37:24 -0500

    Adding gulpfile.js and modification to package.json

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by g.garcia.bermudez on 13/05/2015.
+ */
+var gulp = require('gulp');
+
+gulp.task('hello-world', function(){
+    console.log('Our first hello world gulp task!');
+});
